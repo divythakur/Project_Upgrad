8c427b1b316407d6c1397ea6d241748a5cb73120
Divyanshu <divyanshuthakur75@gmail.com>
2020-05-03 17:47:32 +0530

    homepage in working condition

---
+++ scribbler/scripts/index.js
@@ -0,0 +1,51 @@
+function crossfunc()
+{
+    document.getElementById("modalcontainer").style.display="none";
+    document.getElementById("buttonsincenter").style.display="block";
+    document.getElementById("bg-image").style.opacity="1";
+
+}
+function cross2func()
+{
+    document.getElementById("modal2container").style.display="none";
+    document.getElementById("buttonsincenter").style.display="block";
+    document.getElementById("bg-image").style.opacity="1";
+
+}
+function signupfunction()
+{
+    document.getElementById("buttonsincenter").style.display="none";
+    document.getElementById("modalcontainer").style.display="block";
+    document.getElementById("modal2container").style.display="none";
+    document.getElementById("createpost").style.display="none";
+    document.getElementById("modelinside").style.opacity="1";
+    
+}
+function signinfunc()
+{
+    document.getElementById("modalcontainer").style.display="none";
+    document.getElementById("createpost").style.display="none";
+    document.getElementById("buttonsincenter").style.display="none";
+    document.getElementById("modal2container").style.display="block";
+    document.getElementById("createpost").style.display="none";
+    document.getElementById("model2inside").style.opacity="1";
+    //document.getElementById("bg-image").style.background-color="rgba(0,0,0,0.6)";
+}
+function toggle()
+{
+    document.getElementById("modalcontainer").style.display="block";
+    document.getElementById("modal2container").style.display="none";
+}
+function createpostfunc()
+{
+    document.getElementById("createpost").style.display="block";
+    document.getElementById("buttonsincenter").style.display="none";
+    document.getElementById("modal2container").style.display="none";
+    //document.getElementById("bg-image").style.opacity="0.5";
+}
+function cross3()
+{
+    document.getElementById("createpost").style.display="none";
+    document.getElementById("buttonsincenter").style.display="block";
+    document.getElementById("bg-image").style.opacity="1";
+}
