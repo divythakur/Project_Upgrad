4cfcb5e97dcf3bf4ac0b17397af005d5190b4cf8
Divyanshu <divyanshuthakur75@gmail.com>
2020-05-04 16:15:46 +0530

    Correction in code in div id(buttonsincenter)

--- scribbler/scripts/index.js
+++ scribbler/scripts/index.js
@@ -3,6 +3,10 @@ function crossfunc()
     document.getElementById("modalcontainer").style.display="none";
     document.getElementById("buttonsincenter").style.display="block";
     document.getElementById("bg-image").style.opacity="1";
+    document.getElementById("bg-image").style.background="rgba(0,0,0,0)";
+    document.getElementsByClassName("header-items")[0].style.opacity="1";
+
+
 
 }
 function cross2func()
@@ -10,6 +14,9 @@ function cross2func()
     document.getElementById("modal2container").style.display="none";
     document.getElementById("buttonsincenter").style.display="block";
     document.getElementById("bg-image").style.opacity="1";
+    document.getElementById("bg-image").style.background="rgba(0,0,0,0)";
+    document.getElementsByClassName("header-items")[0].style.opacity="1";
+
 
 }
 function signupfunction()
@@ -18,7 +25,9 @@ function signupfunction()
     document.getElementById("modalcontainer").style.display="block";
     document.getElementById("modal2container").style.display="none";
     document.getElementById("createpost").style.display="none";
-    document.getElementById("modelinside").style.opacity="1";
+    document.getElementById("bg-image").style.background="rgba(0,0,0,0.6)";
+    document.getElementsByClassName("header-items")[0].style.opacity="0.5";
+
     
 }
 function signinfunc()
@@ -29,7 +38,10 @@ function signinfunc()
     document.getElementById("modal2container").style.display="block";
     document.getElementById("createpost").style.display="none";
     document.getElementById("model2inside").style.opacity="1";
-    //document.getElementById("bg-image").style.background-color="rgba(0,0,0,0.6)";
+    document.getElementById("bg-image").style.background="rgba(0,0,0,0.6)";
+    document.getElementsByClassName("header-items")[0].style.opacity="0.5";
+
+    
 }
 function toggle()
 {
@@ -41,11 +53,19 @@ function createpostfunc()
     document.getElementById("createpost").style.display="block";
     document.getElementById("buttonsincenter").style.display="none";
     document.getElementById("modal2container").style.display="none";
-    //document.getElementById("bg-image").style.opacity="0.5";
+    document.getElementById("bg-image").style.background="rgba(0,0,0,0.6)";
+    document.getElementsByClassName("header-items")[0].style.opacity="0.5";
+
+
+   
 }
 function cross3()
 {
     document.getElementById("createpost").style.display="none";
     document.getElementById("buttonsincenter").style.display="block";
     document.getElementById("bg-image").style.opacity="1";
+    document.getElementById("bg-image").style.background="rgba(0,0,0,0)";
+    document.getElementsByClassName("header-items")[0].style.opacity="1";
+
+
 }
