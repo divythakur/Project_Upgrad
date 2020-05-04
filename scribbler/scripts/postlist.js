883f4d298f920eaa35b5fe739e0a23f0685e0a90
Divyanshu <divyanshuthakur75@gmail.com>
2020-05-04 22:37:38 +0530

    modal added & css applied on it

---
+++ scribbler/scripts/postlist.js
@@ -0,0 +1,25 @@
+function areyousure()
+{
+   /* document.getElementById("postcontainer").style.zIndex="0";
+    document.getElementById("trashmodalcontainer").style.display="block";
+    document.getElementById("bg-back").style.background="rgba(0,0,0,0.6)";
+    document.getElementsByClassName("postlist")[0].style.background="rgba(0,0,0,0.1)";
+    document.getElementsByClassName("header-items")[0].style.opacity="0.5";
+    document.getElementsByClassName("nameinpost")[0].style.opacity="0.5";
+    document.getElementsByClassName("nameinpost")[0].style.background="rgba(0,0,0,0.1)";
+    document.getElementsByClassName("postcontent")[0].style.zindex="-1";
+
+
+
+    document.getElementsByClassName("postcontent")[0].style.opacity="0.5";
+
+
+    document.getElementsByClassName("postcontent")[0].style.opacity="0.5";*/
+    document.getElementById("trashmodalcontainer").style.display="block";
+
+
+
+
+
+    
+}
