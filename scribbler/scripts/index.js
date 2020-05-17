f1481f653ab072fc7c4e096b42949e4359cb799d
Divyanshu <divyanshuthakur75@gmail.com>
2020-05-17 19:56:16 +0530

    done relative address of winows.open changed and minor changes in overlay height

--- scribbler/scripts/index.js
+++ scribbler/scripts/index.js
@@ -11,7 +11,7 @@ function signupfunction()
     document.getElementById("modalcontainer").style.display="block";
     document.getElementById("modal2container").style.display="none";
     //overlay properties are changed for the working of modal container (to make modal backgroungd infunctional )
-    document.getElementsByClassName("overlay")[0].style.height="900px";
+    document.getElementsByClassName("overlay")[0].style.height="1000px";
     document.getElementsByClassName("overlay")[0].style.width="100%";
     document.getElementsByClassName("overlay")[0].style.background="rgba(0,0,0,0.6)";
 }
@@ -19,7 +19,7 @@ function signinfunc()
 {
     document.getElementById("modalcontainer").style.display="none";
     document.getElementById("modal2container").style.display="block";
-    document.getElementsByClassName("overlay")[0].style.height="900px";
+    document.getElementsByClassName("overlay")[0].style.height="1000px";
     document.getElementsByClassName("overlay")[0].style.width="100%";
     document.getElementsByClassName("overlay")[0].style.background="rgba(0,0,0,0.6)"  
 }
@@ -31,7 +31,7 @@ function toggle()
 function createpostfunc()
 {
     document.getElementById("createpost").style.display="block";
-    document.getElementsByClassName("overlay")[0].style.height="900px";
+    document.getElementsByClassName("overlay")[0].style.height="1000px";
     document.getElementsByClassName("overlay")[0].style.width="100%";
     document.getElementsByClassName("overlay")[0].style.background="rgba(0,0,0,0.6)";
 }
